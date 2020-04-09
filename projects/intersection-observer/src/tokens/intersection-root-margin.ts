1123073f57b074fceebd15c8a77861a365f1d15a
waterplea <alexander@inkin.ru>
2020-04-09 16:40:22 +0300

    refactor(tokens): add default values and remove @Optional

--- projects/intersection-observer/src/tokens/intersection-root-margin.ts
+++ projects/intersection-observer/src/tokens/intersection-root-margin.ts
@@ -2,4 +2,8 @@ import {InjectionToken} from '@angular/core';
 
 export const INTERSECTION_ROOT_MARGIN = new InjectionToken<string>(
     'rootMargin for IntersectionObserver',
+    {
+        providedIn: 'root',
+        factory: () => '0px 0px 0px 0px',
+    },
 );
--- projects/intersection-observer/src/tokens/intersection-threshold.ts
+++ projects/intersection-observer/src/tokens/intersection-threshold.ts
@@ -2,4 +2,8 @@ import {InjectionToken} from '@angular/core';
 
 export const INTERSECTION_THRESHOLD = new InjectionToken<number | number[]>(
     'threshold for IntersectionObserver',
+    {
+        providedIn: 'root',
+        factory: () => 0,
+    },
 );
